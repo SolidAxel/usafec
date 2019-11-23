5d7e4700ad1f498fa66328deee68090873db4d68
Axel <axegarci@ucsc.edu>
2019-11-22 21:44:18 -0800

    Fix.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css'; /* optional for styling like the :hover pseudo-class */
+import USAMap from "react-usa-map";
+ 
+class App extends Component {
+  /* mandatory */
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
+ 
+  /* optional customization of filling per state and calling custom callbacks per state */
+  statesCustomConfig = () => {
+    return {
+      "CA": {
+        fill: "blue",
+        clickHandler: (event) => console.log('Custom handler for CA', event.target.dataset)
+      },
+    };
+  };
+ 
+  render() {
+    return (
+      <div align="center" class= "mapContainer">
+        <div className="App">
+        <h1 align="center">USA FEC Individual Donations from 1980 to 2020</h1>
+          <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
+        </div>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
